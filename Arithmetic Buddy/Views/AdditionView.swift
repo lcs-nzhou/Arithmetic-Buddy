8c037d270bd66ad56aba229175d5d9ebfee3bfae
Nina Zhou <nzhou@lcs.on.ca>
2024-01-31 11:52:28 -0500

    finish the interface

--- Arithmetic Buddy/Views/AdditionView.swift
+++ Arithmetic Buddy/Views/AdditionView.swift
@@ -8,8 +8,108 @@
 import SwiftUI
 
 struct AdditionView: View {
+    
+    // MARK: Stored properties
+    
+    // The two numbers the user must add
+    @State var firstNumber = Int.random(in: 1...25)
+    @State var secondNumber = Int.random(in: 1...25)
+    
+    @State var givenInput = ""
+    
+    // MARK: Computed properties
+    
+    // The correct sum of the randomly generated numbers
+    var correctSum: Int {
+        return firstNumber + secondNumber
+    }
+    
+    // Feedback to the user
+    @State var feedback = ""
+    
+    // The user interface
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(spacing: 0) {
+            
+            Spacer()
+            
+            HStack {
+                Spacer()
+                Text("\(firstNumber)")
+                    .font(Font.system(size: 50))
+                    .padding(.trailing)
+            }
+            
+            HStack {
+                Text("+")
+                    .font(Font.system(size: 50))
+                Spacer()
+                Text("\(secondNumber)")
+                    .font(Font.system(size: 50))
+                    .padding(.trailing)
+            }
+                        
+            Divider()
+            
+            TextField("Type your answer", text: $givenInput)
+                .font(.largeTitle)
+            Spacer()
+            
+            Button {
+                checkAnswer()
+            } label: {
+                Text("Check Answer")
+            }
+            .buttonStyle(.borderedProminent)
+            
+            Spacer()
+            
+            Text(feedback)
+                .font(
+                    .custom(
+                        "BradleyHandITCTT-Bold",
+                        size: 24.0,
+                        relativeTo: .title3
+                    )
+                )
+            
+            Spacer()
+            
+            Button {
+                reset()
+            } label: {
+                Text("Reset")
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.red)
+        }
+        .padding()
+    }
+    
+    // MARK: Functions
+    
+    // Check whether the user's answer was correct
+    // Provide appropriate feedback
+    func checkAnswer() {
+        guard let selectNumber = Int(givenInput) else {
+            feedback = "Please provide an integer"
+            return
+        }
+        if selectNumber == correctSum {
+            feedback = "correct"
+        } else {
+            feedback = "incorrect"
+        }
+    }
+    
+    // Reset for a new question
+    func reset() {
+        
+        firstNumber = Int.random(in: 1...25)
+        secondNumber = Int.random(in: 1...25)
+        feedback = ""
+        
     }
 }
 
